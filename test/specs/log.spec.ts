c8b246438abb6ba7cbfa6e6ddbb3c15ea132daef
harigithub1 <m.haribabu26@gmail.com>
2025-11-26 16:39:31 +0530

    update

--- test/specs/log.spec.ts
+++ test/specs/log.spec.ts
@@ -1,15 +1,19 @@
 import loginPage from '../../src/pages/login.page';
 import CommonActionsPage from '../../Utilities/CommonActions.page';
 import loginData from '../../testdata/loginData.json';
+import allure from '@wdio/allure-reporter';
+
 
 describe('Mobile App - Login', () => {
   const base = new CommonActionsPage();
-
   it('should login successfully and show welcome message', async () => {
-    await base.waitUntilVisible("loginButton");
+    allure.addStep('Waiting for Login Module');
+    await base.waitUntilVisible("loginModule");
+    allure.addStep('Entering username & password');
     await loginPage.login(loginData.validUser.username, loginData.validUser.password);
     await base.isVisible("EmailError");
     await base.isVisible("PasswordError");
-    await base.takeScreenshot('after');
+    allure.addStep('Checking Login Button visibility');
+    await base.waitUntilVisible("loginButton");
   });
 });
--- test/specs/login.spec.ts
+++ test/specs/login.spec.ts
@@ -1,15 +1,17 @@
 import loginPage from '../../src/pages/login.page';
 import CommonActionsPage from '../../Utilities/CommonActions.page';
 import loginData from '../../testdata/loginData.json';
-
+import allure from '@wdio/allure-reporter';
 
 describe('Mobile App - Login Test', () => {
   const base = new CommonActionsPage();
 
   it('should login successfully and show welcome message', async () => {
-    
+    allure.addStep('Waiting for Login Module');
     await base.waitUntilVisible("loginModule");
+    allure.addStep('Entering username & password');
     await loginPage.login(loginData.validUser.username, loginData.validUser.password);
+    allure.addStep('Validating error message');
     await base.isVisible("EmailError");    
   });
 });
